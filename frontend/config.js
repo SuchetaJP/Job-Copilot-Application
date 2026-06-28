d4c543956b6f893fa9f49a6ee7b2d5012956e2f1
SuchetaJP <suchetapalkar@gmail.com>
2026-06-28 20:26:51 +0530

    changed config for API_BASE_URL and added Config.js file

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+window.API_BASE_URL =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
+        ? "http://127.0.0.1:8000"
+        : "https://job-copilot-application.onrender.com";
